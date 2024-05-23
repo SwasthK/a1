ea74d4142fba63fa1599d8b6a35fbe642de01d49
SwasthK <220981@sdmcujire.in>
2024-05-24 01:21:47 +0530

    UI update

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -7,15 +7,33 @@ import "./App.css";
 function App() {
   return (
     <>
+     
+      <div className="text-4xl sm:text-4xl lg:text-8xl font-bold mt-8">
+        <h1 className="text-[#CAA6F7]">
+          Happy Birthday <span className="text-[#AAE0BF]">Hebbar</span>🥳🎈
+        </h1>
+      </div>
+      <div className="grid-cols-4 grid gap-5">
       <div className="flex justify-center">
         <div className=" w-72 sm:w-96">
           <img src="/a1.jpg" alt="Vite logo" className="border-2 rounded-[15px]" />
         </div>
       </div>
-      <div className="text-4xl sm:text-4xl lg:text-8xl font-bold mt-8">
-        <h1 className="text-[#CAA6F7]">
-          Happy Birthday <span className="text-[#AAE0BF]">Hebbar</span>🥳🎈
-        </h1>
+      <div className="flex justify-center mb-6">
+        <div className=" w-72 sm:w-96">
+          <img src="/a2.jpg" alt="Vite logo" className="border-2 rounded-[15px]" />
+        </div>
+      </div>
+      <div className="flex justify-center mb-6">
+        <div className=" w-72 sm:w-96">
+          <img src="/a3.jpg" alt="Vite logo" className="border-2 rounded-[15px]" />
+        </div>
+      </div>
+      <div className="flex justify-center mb-6">
+        <div className=" w-72 sm:w-96 ">
+          <img src="/4.jpg" alt="Vite logo" className="border-2 rounded-[15px]" />
+        </div>
+      </div>
       </div>
     </>
   );
